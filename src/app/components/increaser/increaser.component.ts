246431cf6b0365a67a6b03c4bc0866267c7a17ae
Adrian Rojas <adrian.rojas@tergar.org>
2024-04-03 20:09:51 -0600

    Update: add comunication betwen 2 components, father to children(@Input) && children to father(@Output)

--- src/app/components/increaser/increaser.component.ts
+++ src/app/components/increaser/increaser.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-increaser',
@@ -8,18 +8,30 @@ import { Component, Input } from '@angular/core';
 })
 export class IncreaserComponent {
 
-  //If you need a diferent name for the external variable definition, you can use the @Input('external_name')
-  // @Input('progress_input') progress: number = 50;
-  @Input() progress: number = 50;
+  //With external variable name
+  @Input('value') progress_value: number = 50; //@Input for recive the value from the father component
+  @Output('value') progress_output: EventEmitter<number> = new EventEmitter(); //@Output for emit event(send value)to the father component
+
+  //Without external variable name
+  @Input() type_button: string = 'btn-primary';
 
   changeProgress(number: number){
 
-    if(this.progress >= 100 && number > 0){
-      return this.progress = 100;
-    } else if(this.progress <= 0 && number <0 ){
-      return this.progress = 0;
+    if(this.progress_value >= 100 && number > 0){
+
+      this.progress_output.emit(100);
+      return this.progress_value = 100;
+
+    } else if(this.progress_value <= 0 && number <0 ){
+
+      this.progress_output.emit(0);
+      return this.progress_value = 0;
     }
-    return this.progress = this.progress + number;
+
+    this.progress_value = this.progress_value + number;
+    this.progress_output.emit(this.progress_value);
+    return  this.progress_value;
+
   }
 
 }
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class ProgressComponent {
 
+  progress1: number = 10;
+  progress2: number = 30;
+
+  //This is for defining the value of the progress bar with percentage value
+  get getProgress1(){
+    return `${this.progress1}%`;
+  }
+
+  get getProgress2(){
+    return `${this.progress2}%`;
+  }
+
 }
