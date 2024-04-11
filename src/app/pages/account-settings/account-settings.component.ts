8b3a3f3300d1a64d6884ba8b3f8d3593277f3728
Adrian Rojas <adrian.rojas@tergar.org>
2024-04-10 20:07:18 -0600

    Update: change the them icon when other theme is selected

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-account-settings',
@@ -6,13 +6,38 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class AccountSettingsComponent {
+export class AccountSettingsComponent implements OnInit{
 
-  public theme = document.querySelector('#theme-id');
+  //Refs DOM
+  public theme = document.querySelector('#theme-id') as HTMLLinkElement;
+  public theme_links = document.getElementsByClassName('selector');
+
+
+  ngOnInit(): void {
+    this.checkCurrentTheme();
+  }
 
   changeTheme(new_theme: string) {
     localStorage.setItem('theme', new_theme);
     this.theme?.setAttribute('href', `./assets/css/colors/${new_theme}.css`)
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme(){
+
+    var links_arr = Array.from(this.theme_links);
+
+    links_arr.map(link_option =>{
+
+      link_option.classList.remove('working');
+
+      const btnTheme = link_option.getAttribute('data-theme')
+      const currentTheme = this.theme.getAttribute('href');
+
+      if(`./assets/css/colors/${btnTheme}.css` === currentTheme){
+        link_option.classList.add('working');
+      }
+    });
   }
 
 }
