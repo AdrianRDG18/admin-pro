d79ceb8a17914e8a19fa40cc0ce05471b9871772
Adrian Rojas <adrian.rojas@tergar.org>
2024-04-16 08:44:14 -0600

    Update: re-init theme styles in pages.component | Use of scripts imported in index.html

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
+
+declare function customInitStyleFunctions(): any;
 
 @Component({
   selector: 'app-pages',
@@ -7,8 +9,12 @@ import { SettingsService } from '../services/settings.service';
   styles: [
   ]
 })
-export class PagesComponent{
+export class PagesComponent implements OnInit{
 
   constructor(private settingsService: SettingsService){}
+
+  ngOnInit(): void {
+    customInitStyleFunctions();
+  }
 
 }
