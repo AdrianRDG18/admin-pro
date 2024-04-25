4138c60cc7a52784d151c954f60263e518531168
Adrian Rojas <adrian.rojas@tergar.org>
2024-04-25 08:41:04 -0600

    Update: observable structure

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
   ]
 })
 export class RxjsComponent {
+
+  constructor() {
+    
+    const obs$ = new Observable( observer =>{
+      let i = -1;
+      const interval = setInterval( () => {
+        i++;
+        observer.next(i);
+        if(i === 4){
+          clearInterval(interval);
+          observer.complete();
+        }
+      }, 1000)
+    });
+
+    obs$.subscribe( 
+        valor => console.log('Subs', valor),
+        (error) => console.error("Error: ", error),
+        () => console.info('El observador termino')
+    );
+
+  }
 
 }
