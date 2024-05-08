3cf4a5ba878661ed63ac69d99f645357fe4485c9
Adrian Rojas <adrian.rojas@tergar.org>
2024-05-08 12:05:02 -0600

    Update: add ngOnDestroy to breadcrumbs component to get name route

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivationEnd, Router } from '@angular/router';
-import { filter, map } from 'rxjs';
+import { Subscription, filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -8,25 +8,29 @@ import { filter, map } from 'rxjs';
   styles: [
   ]
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
 
   title: string = '';
+  titleSubs$: Subscription;
 
   constructor(private router: Router) {
-    this.extractTitleRoute();
+    this.titleSubs$ = this.extractTitleRoute()
+                          .subscribe( ({title}) => {
+                            this.title = title;
+                            document.title = `AdminPro - ${title}`;
+                          });;
+  }
+  ngOnDestroy(): void {
+    this.titleSubs$.unsubscribe();
   }
   
   extractTitleRoute(){
-    this.router.events
-    .pipe(
-      filter( (event: any) => event instanceof ActivationEnd),
-      filter( (event:ActivationEnd) => event.snapshot.firstChild === null ),
-      map( (event: ActivationEnd) => event.snapshot.data)
-    )
-    .subscribe( ({title}) => {
-      this.title = title;
-      document.title = `AdminPro - ${title}`;
-    });
+    return this.router.events
+      .pipe(
+        filter( (event: any) => event instanceof ActivationEnd),
+        filter( (event:ActivationEnd) => event.snapshot.firstChild === null ),
+        map( (event: ActivationEnd) => event.snapshot.data)
+      )
   }
 
 }
