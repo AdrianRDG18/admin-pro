60f946ccda69189338d7da385f4f477415ddb4f5
Adrian Rojas <adrian.rojas@tergar.org>
2024-06-17 08:36:03 -0600

    Feature: create reactive form in user register componentet & assign it into html

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: [ './register.component.css' ]
 })
 export class RegisterComponent {
+
+  public createUserForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required,Validators.email]],
+    password: ['', Validators.required],
+    password_confirmation: ['', Validators.required],
+    terms: [false, Validators.requiredTrue]
+  });
+
+  constructor(private fb: FormBuilder){}
+
+  creatrUser(){
+    console.log(this.createUserForm.value);
+  }
 
 }
