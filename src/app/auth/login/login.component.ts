e2adc45e893af8600e504f2219b14dc13ab3f74f
Adrian Rojas <adrian.rojas@tergar.org>
2024-06-27 08:55:21 -0600

    Feature: remember button working

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AlertService } from 'src/app/services/alert.service';
@@ -10,12 +10,12 @@ import Swal from 'sweetalert2';
   templateUrl: './login.component.html',
   styleUrls: [ './login.component.css' ]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
   private formSubmitted: boolean = false;
 
   public loginForm: FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
+    email: new FormControl(localStorage.getItem('email') || '', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
     remember: new FormControl(false)
   });
@@ -24,6 +24,12 @@ export class LoginComponent {
               private _userService: UserService,
               private _swal: AlertService
   ){}
+
+  ngOnInit(): void {
+    if(localStorage.getItem('email')){
+      this.loginForm.get('remember')?.setValue(true);
+    }
+  }
 
   login(){
 
@@ -36,6 +42,7 @@ export class LoginComponent {
       this._userService.login(this.loginForm.value)
           .subscribe({
             next: (resp) => {
+              (this.loginForm.get('remember')?.value) ? localStorage.setItem('email', this.loginForm.get('email')?.value) : localStorage.removeItem('email');
               console.log(resp);
               // this._router.navigateByUrl('/dashboard');
             },
